f73f6ea9ea71bc714f48cd112c399a590d7ffc68
Thanh Nguyen <72685335+huscongao1692003@users.noreply.github.com>
2024-06-05 18:45:56 +0700

    lol

--- Assets/Creator Kit - FPS/Scripts/System/Controller.cs
+++ Assets/Creator Kit - FPS/Scripts/System/Controller.cs
@@ -1,20 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
-
-[System.Serializable]
-public class AmmoInventoryEntry
-{
-    public int ammoType;
-    public int amount = 0;
-}
 
 public class Controller : MonoBehaviour
 {
     public static Controller Instance { get; protected set; }
 
     public OVRCameraRig CameraRig;
+    private Weapon _weapon;
+    public Controller(Weapon weapon)
+    {
+        _weapon = weapon;
+    }
 
 
     public OVRInput.Controller LeftController;
@@ -31,106 +28,16 @@ public class Controller : MonoBehaviour
     public AudioClip JumpingAudioClip;
     public AudioClip LandingAudioClip;
 
-    private float m_VerticalSpeed = 0.0f;
-    private bool m_IsPaused = false;
-    private int m_CurrentWeapon;
-
-    private OVRInput.Controller m_CharacterController;
-    private bool m_Grounded;
-    private float m_GroundedTimer;
-    private float m_SpeedAtJump = 0.0f;
-
-    private List<Weapon> m_Weapons = new List<Weapon>();
-    private Dictionary<int, int> m_AmmoInventory = new Dictionary<int, int>();
-
-    void Awake()
-    {
-        Instance = this;
-    }
-
-    void Start()
-    {
-        m_IsPaused = false;
-        m_Grounded = true;
-
-        m_CharacterController = GetComponent<OVRInput.Controller>();
-
-        
-      
-        m_CurrentWeapon = -1;
-        ChangeWeapon(0);
-
-    
-    }
-
+   
     void Update()
     {
-        bool wasGrounded = m_Grounded;
-        bool loosedGrounding = false;
-
-        
-        if (!m_IsPaused)
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
         {
-            if (m_Grounded && OVRInput.GetDown(OVRInput.Button.One))
-            {
-                m_VerticalSpeed = JumpSpeed;
-                m_Grounded = false;
-                loosedGrounding = true;
-                FootstepPlayer.PlayClip(JumpingAudioClip, 0.8f, 1.1f);
-            }
-
-          
-            float actualSpeed = PlayerSpeed;
-
-            if (loosedGrounding)
-            {
-                m_SpeedAtJump = actualSpeed;
-            }
-
-       
-
-            Vector2 secondaryAxis = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
-            transform.Rotate(0, secondaryAxis.x * 45.0f * Time.deltaTime, 0);
-
-            
-
-           
-            if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstick))
-            {
-                ChangeWeapon(m_CurrentWeapon + 1);
-            }
-        }
-
-        m_VerticalSpeed = m_VerticalSpeed - 10.0f * Time.deltaTime;
-        if (m_VerticalSpeed < -10.0f)
-            m_VerticalSpeed = -10.0f;
-      
-        if (!wasGrounded && m_Grounded)
-        {
-            FootstepPlayer.PlayClip(LandingAudioClip, 0.8f, 1.1f);
+            _weapon.Shoot();
         }
     }
 
-    
 
-    void ChangeWeapon(int number)
-    {
-        if (m_CurrentWeapon != -1)
-        {
-            m_Weapons[m_CurrentWeapon].PutAway();
-            m_Weapons[m_CurrentWeapon].gameObject.SetActive(false);
-        }
-
-        m_CurrentWeapon = number;
-
-        if (m_CurrentWeapon < 0)
-            m_CurrentWeapon = m_Weapons.Count - 1;
-        else if (m_CurrentWeapon >= m_Weapons.Count)
-            m_CurrentWeapon = 0;
-
-        m_Weapons[m_CurrentWeapon].gameObject.SetActive(true);
-        m_Weapons[m_CurrentWeapon].Selected();
-    }
 
 
     public void PlayFootstep()
--- Assets/Creator Kit - FPS/Scripts/System/Weapon.cs
+++ Assets/Creator Kit - FPS/Scripts/System/Weapon.cs
@@ -7,179 +7,240 @@ using UnityEditor;
 
 public class Weapon : MonoBehaviour
 {
-    static RaycastHit[] s_HitInfoBuffer = new RaycastHit[8];
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject bulletPosision;
+    [SerializeField] private float shotDelay = 0.2f;
+    [Range(0,3000),SerializeField] private float bulletSpeed;
+    [Space,SerializeField] private AudioSource audioSource;
 
-    public enum TriggerType
+    private float lastShot;
+
+    public void Shoot()
     {
-        Auto,
-        Manual
-    }
 
-    public enum WeaponType
-    {
-        Raycast,
-        Projectile
-    }
-
-
-
-    [System.Serializable]
-    public class AdvancedSettings
-    {
-        public float spreadAngle = 0.0f;
-        public int projectilePerShot = 1;
-        public float screenShakeMultiplier = 1.0f;
-    }
-
-    public TriggerType triggerType = TriggerType.Manual;
-    public WeaponType weaponType = WeaponType.Raycast;
-    public float fireRate = 0.5f;
-    public int clipSize = 4;
-    public float damage = 1.0f;
-    public int ammoType = -1;
-    public Transform EndPoint;
-    public AdvancedSettings advancedSettings;
-
-    [Header("Audio Clips")]
-    public AudioClip FireAudioClip;
-    public AudioClip ReloadAudioClip;
-
-    [Header("Visual Settings")]
-    public LineRenderer PrefabRayTrail;
-
-    private bool m_TriggerDown;
-    private bool m_ShotDone;
-    private int m_ClipContent;
-    private Animator m_Animator;
-    private AudioSource m_Source;
-    private float m_ShotTimer = -1.0f;
-    private List<ActiveTrail> m_ActiveTrails = new List<ActiveTrail>();
-    private Queue<GameObject> m_ProjectilePool = new Queue<GameObject>();
-
-    private OVRGrabbable m_Grabbable;
-
-    class ActiveTrail
-    {
-        public LineRenderer renderer;
-        public Vector3 direction;
-        public float remainingTime;
-    }
-
-    void Awake()
-    {
-        m_Animator = GetComponentInChildren<Animator>();
-        m_Source = GetComponentInChildren<AudioSource>();
-        m_ClipContent = clipSize;
-
-        m_Grabbable = GetComponent<OVRGrabbable>();
-
-        if (PrefabRayTrail != null)
-        {
-            const int trailPoolSize = 16;
-            PoolSystem.Instance.InitPool(PrefabRayTrail, trailPoolSize);
-        }
+        if (lastShot > Time.time) return;
+        lastShot = Time.time + shotDelay;
+        var bulletPrefab = Instantiate(bullet,bulletPosision.transform.position, bulletPosision.transform.rotation);
+        var bulletRB = bulletPrefab.GetComponent<Rigidbody>();
+        var direction = bulletPrefab.transform.TransformDirection(Vector3.forward);
+        bulletRB.AddForce(direction*bulletSpeed);
+        Destroy(bulletPrefab, 5f);
 
     }
-
-    public void PutAway()
+    private void Update()
     {
-        m_Animator.WriteDefaultValues();
-        foreach (var activeTrail in m_ActiveTrails)
-        {
-            activeTrail.renderer.gameObject.SetActive(false);
-        }
-        m_ActiveTrails.Clear();
-    }
-
-    public void Selected()
-    {
-       
-        m_ShotDone = false;
-
-        WeaponInfoUI.Instance.UpdateWeaponName(this);
-        WeaponInfoUI.Instance.UpdateAmmoAmount(1000);
-
-        m_Animator.SetTrigger("selected");
-    }
-
-    public void Fire()
-    {
-        if (!m_Grabbable.isGrabbed || m_ShotTimer > 0 || m_ClipContent == 0)
-            return;
-
-        m_ClipContent -= 1;
-        m_ShotTimer = fireRate;
-        m_Animator.SetTrigger("fire");
-        m_Source.pitch = Random.Range(0.7f, 1.0f);
-        m_Source.PlayOneShot(FireAudioClip);
-
-            for (int i = 0; i < advancedSettings.projectilePerShot; ++i)
-            {
-                RaycastShot();
-            }
-        
-     
-    }
-
-    void RaycastShot()
-    {
-        float spreadRatio = advancedSettings.spreadAngle / OVRManager.instance.GetComponent<Camera>().fieldOfView;
-        Vector2 spread = spreadRatio * Random.insideUnitCircle;
-        RaycastHit hit;
-        Ray r = new Ray(EndPoint.position, EndPoint.forward + new Vector3(spread.x, spread.y, 0));
-        Vector3 hitPosition = r.origin + r.direction * 200.0f;
-
-        if (Physics.Raycast(r, out hit, 1000.0f, ~(1 << 9), QueryTriggerInteraction.Ignore))
-        {
-            Renderer renderer = hit.collider.GetComponentInChildren<Renderer>();
-            ImpactManager.Instance.PlayImpact(hit.point, hit.normal, renderer == null ? null : renderer.sharedMaterial);
-
-            if (hit.distance > 5.0f)
-                hitPosition = hit.point;
-
-            if (hit.collider.gameObject.layer == 10)
-            {
-                Target target = hit.collider.gameObject.GetComponent<Target>();
-                target.Got(damage);
-            }
-        }
-
-        if (PrefabRayTrail != null)
-        {
-            var pos = new Vector3[] { EndPoint.position, hitPosition };
-            var trail = PoolSystem.Instance.GetInstance<LineRenderer>(PrefabRayTrail);
-            trail.gameObject.SetActive(true);
-            trail.SetPositions(pos);
-            m_ActiveTrails.Add(new ActiveTrail()
-            {
-                remainingTime = 0.3f,
-                direction = (pos[1] - pos[0]).normalized,
-                renderer = trail
-            });
-        }
-    }
-
-  
-    void Update()
-    {
-        if (m_ShotTimer >= 0)
-        {
-            m_ShotTimer -= Time.deltaTime;
-        }
-
         if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
         {
-            if (triggerType == TriggerType.Auto || !m_ShotDone)
-            {
-                Fire();
-                m_ShotDone = true;
-            }
+            Shoot();
         }
     }
-
-    public void ReturnProjectileToPool(GameObject projectile)
+    private void GunShotAudio()
     {
-        projectile.SetActive(false);
-        m_ProjectilePool.Enqueue(projectile);
+        var random = Random.Range(0.8f, 1.2f);
+        audioSource.pitch = random;
+        audioSource.Play();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //static RaycastHit[] s_HitInfoBuffer = new RaycastHit[8];
+
+    //public enum TriggerType
+    //{
+    //    Auto,
+    //    Manual
+    //}
+
+    //public enum WeaponType
+    //{
+    //    Raycast,
+    //    Projectile
+    //}
+
+
+
+    //[System.Serializable]
+    //public class AdvancedSettings
+    //{
+    //    public float spreadAngle = 0.0f;
+    //    public int projectilePerShot = 1;
+    //    public float screenShakeMultiplier = 1.0f;
+    //}
+
+    //public TriggerType triggerType = TriggerType.Manual;
+    //public WeaponType weaponType = WeaponType.Raycast;
+    //public float fireRate = 0.5f;
+    //public int clipSize = 4;
+    //public float damage = 1.0f;
+    //public int ammoType = -1;
+    //public Transform EndPoint;
+    //public AdvancedSettings advancedSettings;
+
+    //[Header("Audio Clips")]
+    //public AudioClip FireAudioClip;
+    //public AudioClip ReloadAudioClip;
+
+    //[Header("Visual Settings")]
+    //public LineRenderer PrefabRayTrail;
+
+    //private bool m_TriggerDown;
+    //private bool m_ShotDone;
+    //private int m_ClipContent;
+    //private Animator m_Animator;
+    //private AudioSource m_Source;
+    //private float m_ShotTimer = -1.0f;
+    //private List<ActiveTrail> m_ActiveTrails = new List<ActiveTrail>();
+    //private Queue<GameObject> m_ProjectilePool = new Queue<GameObject>();
+
+    //private OVRGrabbable m_Grabbable;
+
+    //class ActiveTrail
+    //{
+    //    public LineRenderer renderer;
+    //    public Vector3 direction;
+    //    public float remainingTime;
+    //}
+
+    //void Awake()
+    //{
+    //    m_Animator = GetComponentInChildren<Animator>();
+    //    m_Source = GetComponentInChildren<AudioSource>();
+    //    m_ClipContent = clipSize;
+
+    //    m_Grabbable = GetComponent<OVRGrabbable>();
+
+    //    if (PrefabRayTrail != null)
+    //    {
+    //        const int trailPoolSize = 16;
+    //        PoolSystem.Instance.InitPool(PrefabRayTrail, trailPoolSize);
+    //    }
+
+    //}
+
+    //public void PutAway()
+    //{
+    //    m_Animator.WriteDefaultValues();
+    //    foreach (var activeTrail in m_ActiveTrails)
+    //    {
+    //        activeTrail.renderer.gameObject.SetActive(false);
+    //    }
+    //    m_ActiveTrails.Clear();
+    //}
+
+    //public void Selected()
+    //{
+       
+    //    m_ShotDone = false;
+
+    //    WeaponInfoUI.Instance.UpdateWeaponName(this);
+    //    WeaponInfoUI.Instance.UpdateAmmoAmount(1000);
+
+    //    m_Animator.SetTrigger("selected");
+    //}
+
+    //public void Fire()
+    //{
+    //    if (!m_Grabbable.isGrabbed || m_ShotTimer > 0 || m_ClipContent == 0)
+    //        return;
+
+    //    m_ClipContent -= 1;
+    //    m_ShotTimer = fireRate;
+    //    m_Animator.SetTrigger("fire");
+    //    m_Source.pitch = Random.Range(0.7f, 1.0f);
+    //    m_Source.PlayOneShot(FireAudioClip);
+
+    //        for (int i = 0; i < advancedSettings.projectilePerShot; ++i)
+    //        {
+    //            RaycastShot();
+    //        }
+        
+     
+    //}
+
+    //void RaycastShot()
+    //{
+    //    float spreadRatio = advancedSettings.spreadAngle / OVRManager.instance.GetComponent<Camera>().fieldOfView;
+    //    Vector2 spread = spreadRatio * Random.insideUnitCircle;
+    //    RaycastHit hit;
+    //    Ray r = new Ray(EndPoint.position, EndPoint.forward + new Vector3(spread.x, spread.y, 0));
+    //    Vector3 hitPosition = r.origin + r.direction * 200.0f;
+
+    //    if (Physics.Raycast(r, out hit, 1000.0f, ~(1 << 9), QueryTriggerInteraction.Ignore))
+    //    {
+    //        Renderer renderer = hit.collider.GetComponentInChildren<Renderer>();
+    //        ImpactManager.Instance.PlayImpact(hit.point, hit.normal, renderer == null ? null : renderer.sharedMaterial);
+
+    //        if (hit.distance > 5.0f)
+    //            hitPosition = hit.point;
+
+    //        if (hit.collider.gameObject.layer == 10)
+    //        {
+    //            Target target = hit.collider.gameObject.GetComponent<Target>();
+    //            target.Got(damage);
+    //        }
+    //    }
+
+    //    if (PrefabRayTrail != null)
+    //    {
+    //        var pos = new Vector3[] { EndPoint.position, hitPosition };
+    //        var trail = PoolSystem.Instance.GetInstance<LineRenderer>(PrefabRayTrail);
+    //        trail.gameObject.SetActive(true);
+    //        trail.SetPositions(pos);
+    //        m_ActiveTrails.Add(new ActiveTrail()
+    //        {
+    //            remainingTime = 0.3f,
+    //            direction = (pos[1] - pos[0]).normalized,
+    //            renderer = trail
+    //        });
+    //    }
+    //}
+
+  
+    //void Update()
+    //{
+    //    if (m_ShotTimer >= 0)
+    //    {
+    //        m_ShotTimer -= Time.deltaTime;
+    //    }
+
+    //    if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+    //    {
+    //        if (!m_ShotDone)
+    //        {
+    //            Fire();
+    //            m_ShotDone = true;
+    //        }
+    //    }
+    //}
+
+    //public void ReturnProjectileToPool(GameObject projectile)
+    //{
+    //    projectile.SetActive(false);
+    //    m_ProjectilePool.Enqueue(projectile);
+    //}
 }
